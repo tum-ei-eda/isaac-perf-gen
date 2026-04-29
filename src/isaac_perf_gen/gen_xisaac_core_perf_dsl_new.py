1bb81dd12d9709a52883b4ae1eec28b1f4c846dc
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-04-29 12:26:55 +0200

    gen_xisaac_core_perf_dsl_new: update

--- src/isaac_perf_gen/gen_xisaac_core_perf_dsl_new.py
+++ src/isaac_perf_gen/gen_xisaac_core_perf_dsl_new.py
@@ -51,13 +51,14 @@ def get_permutations(d):
 
 
 def get_templates_base(core: Optional[str] = None):
-    templates_path = files(
-        "isaac_perf_gen.templates"
-    )  # .joinpath("message.eml").read_text()
+    templates_path = files("isaac_perf_gen.templates")  # .joinpath("message.eml").read_text()
     # print("templates_path", templates_path)
     if core is not None:
         templates_path = templates_path.joinpath(core)
         # print("templates_path", templates_path)
+    else:
+        templates_path = templates_path.joinpath("/")
+    # print("templates_path", templates_path, type(templates_path))
     assert Path(templates_path).is_dir()
     return templates_path
 
@@ -65,7 +66,8 @@ def get_templates_base(core: Optional[str] = None):
 def lookup_template(inp: str, core: Optional[str] = None, suffix: Optional[str] = None):
     # print("lookup_template", inp, core)
     assert inp is not None
-    assert core is not None
+    inp = str(inp)
+    # assert core is not None
 
     templates_path = get_templates_base(core=core)
     if Path(inp).is_file():
@@ -79,20 +81,20 @@ def lookup_template(inp: str, core: Optional[str] = None, suffix: Optional[str] 
         inp += ".mako"
     # print("inp", inp)
     template_path = templates_path.joinpath(inp)
+    template_path = Path(template_path)
     # print("template_path", template_path)
-    assert Path(template_path).is_file(), f"Not a file: {template_path}"
+    if not template_path.is_file():
+        if core is not None:
+            return lookup_template(inp, core=None, suffix=suffix)
+        assert False, f"Not a file: {template_path}"
     return str(Path(template_path))
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--template", default=None, help="Base MAKO Template")
-    parser.add_argument(
-        "-o", "--output", default=None, help="Output .core_perf_dsl file path"
-    )
-    parser.add_argument(
-        "--monitor-template", default=None, help="Mako template for monitor description"
-    )
+    parser.add_argument("-o", "--output", default=None, help="Output .core_perf_dsl file path")
+    parser.add_argument("--monitor-template", default=None, help="Mako template for monitor description")
     parser.add_argument(
         "--monitor-dest",
         default=None,
@@ -103,15 +105,11 @@ def main():
         default=None,
         help="Directory containing generated INI files (and mako templates)",
     )
-    parser.add_argument(
-        "--uarchs-dest", default=None, help="Output CSV file for uarch names"
-    )
-    parser.add_argument(
-        "-c", "--core", required=True, choices=["cv32e40p", "cva6"], help="Base core"
-    )
-    parser.add_argument(
-        "--temp-dir", default=None, help="Optional path to persistent temp dir"
-    )
+    parser.add_argument("--uarchs-dest", default=None, help="Output CSV file for uarch names")
+    parser.add_argument("-c", "--core", required=True, choices=["cv32e40p", "cva6"], help="Base core")
+    parser.add_argument("-u", "--uarch", default=None, help="Perf model name")
+    parser.add_argument("-a", "--etiss-arch", default="XIsaacCore", help="Base ETISS arch name")
+    parser.add_argument("--temp-dir", default=None, help="Optional path to persistent temp dir")
     parser.add_argument("--hls-dir", default=None, help="Path to hls output dir")
     # parser.add_argument("--hls-schedules", default=None, help="Path to hls_schedules.csv")
     # parser.add_argument("--hls-yaml", default=None, help="Path to ISAX_XIsaac.yaml")
@@ -119,12 +117,18 @@ def main():
     parser.add_argument("--variants", default=None, help="Filter variants")
     parser.add_argument("--index-yaml", default=None, help="Path to XISAAC index.yml")
     parser.add_argument("--parts-only", action="store_true", help="Only generate parts")
-    parser.add_argument(
-        "--render-only", action="store_true", help="Only render final output"
-    )
+    parser.add_argument("--render-only", action="store_true", help="Only render final output")
     args = parser.parse_args()
     core = args.core
-    core_name = "XIsaacCore"
+    core_name = args.etiss_arch
+
+    uarch_name = args.uarch
+    if uarch_name is None:
+        core is not None
+        core_upper = core.upper()
+        default_uarch_name = f"{core_upper}XISAAC"
+        lookup_uarch_name = {}
+        uarch_name = lookup_uarch_name.get("CV32E40PXISAAC", default_uarch_name)
 
     @contextmanager
     def temp_dir_content(enter_result=None):
@@ -135,7 +139,8 @@ def main():
                 yield Path(tmpdirname)
 
     templates_path = get_templates_base(core=core)
-    template_dirs = [".", "templates/", templates_path, f"{templates_path}/parts"]
+    common_path = get_templates_base(core="common")
+    template_dirs = [".", "templates/", templates_path, f"{templates_path}/parts", common_path, f"{common_path}/parts"]
     lookup_dirs = defaultdict(list)
     lookup_dirs2 = []
     xlen = None
@@ -181,24 +186,16 @@ def main():
             variants = {}
             variant_extras = {}
             if hls_schedules_df is not None:
-                hls_schedules_df["SG"] = hls_schedules_df["config"].apply(
-                    lambda x: int(x.split("_")[1])
-                )
-            hls_selected_schedule_metrics_csv = (
-                Path(args.hls_dir) / "hls_selected_schedule_metrics.csv"
-            )
-            assert (
-                hls_selected_schedule_metrics_csv.is_file()
-            ), f"Missing: {hls_selected_schedule_metrics_csv}"
+                hls_schedules_df["SG"] = hls_schedules_df["config"].apply(lambda x: int(x.split("_")[1]))
+            hls_selected_schedule_metrics_csv = Path(args.hls_dir) / "hls_selected_schedule_metrics.csv"
+            assert hls_selected_schedule_metrics_csv.is_file(), f"Missing: {hls_selected_schedule_metrics_csv}"
             hls_variants_df = pd.read_csv(hls_selected_schedule_metrics_csv)
             num_variants = len(hls_variants_df)
             # print("hls_variants_df")
             print(hls_variants_df)
             # print("num_variants", num_variants)
             if variants_filter:
-                hls_variants_df = hls_variants_df[
-                    hls_variants_df["Variant idx"].isin(variants_filter)
-                ]
+                hls_variants_df = hls_variants_df[hls_variants_df["Variant idx"].isin(variants_filter)]
             print("hls_variants_df")
             print(hls_variants_df)
             for idx, variant_row in hls_variants_df.iterrows():
@@ -218,19 +215,10 @@ def main():
                     total_area_estimate,
                 )
                 if variant_name is not None:
-                    selected_solutions_yaml = (
-                        Path(args.hls_dir)
-                        / "output"
-                        / variant_name
-                        / "selected_solutions.yaml"
-                    )
+                    selected_solutions_yaml = Path(args.hls_dir) / "output" / variant_name / "selected_solutions.yaml"
                 else:
-                    selected_solutions_yaml = (
-                        Path(args.hls_dir) / "output" / "selected_solutions.yaml"
-                    )
-                assert (
-                    selected_solutions_yaml.is_file()
-                ), f"Missing: {selected_solutions_yaml}"
+                    selected_solutions_yaml = Path(args.hls_dir) / "output" / "selected_solutions.yaml"
+                assert selected_solutions_yaml.is_file(), f"Missing: {selected_solutions_yaml}"
                 with open(selected_solutions_yaml) as f:
                     selected_solutions = yaml.safe_load(f)
                 variant = selected_solutions
@@ -246,12 +234,7 @@ def main():
                 # else:
                 #     hls_yaml = Path(args.hls_yaml)
                 if variant_name is not None:
-                    hls_yaml = (
-                        Path(args.hls_dir)
-                        / "output"
-                        / variant_name
-                        / "ISAX_XIsaac.yaml"
-                    )
+                    hls_yaml = Path(args.hls_dir) / "output" / variant_name / "ISAX_XIsaac.yaml"
                 else:
                     hls_yaml = Path(args.hls_dir) / "output" / "ISAX_XIsaac.yaml"
                 assert hls_yaml.is_file(), f"Missing: {hls_yaml}"
@@ -260,19 +243,12 @@ def main():
                     # print("hls_data", hls_data)
 
                 def apply_selection(hls_schedules_df, selected_solutions):
-                    configs = [
-                        f"SG_{x['sharing_group']}_SOL_IDX_{x['solution_idx']}"
-                        for x in selected_solutions
-                    ]
+                    configs = [f"SG_{x['sharing_group']}_SOL_IDX_{x['solution_idx']}" for x in selected_solutions]
                     # print("configs", configs)
-                    hls_schedules_df_ = hls_schedules_df[
-                        hls_schedules_df["config"].isin(configs)
-                    ]
+                    hls_schedules_df_ = hls_schedules_df[hls_schedules_df["config"].isin(configs)]
                     return hls_schedules_df_
 
-                hls_schedules_df_ = apply_selection(
-                    hls_schedules_df, selected_solutions
-                )
+                hls_schedules_df_ = apply_selection(hls_schedules_df, selected_solutions)
                 # print("hls_schedules_df_", hls_schedules_df_)
                 instr_latencies = {}
                 sg2ii = {}
@@ -295,9 +271,7 @@ def main():
                     else:
                         lats = ast.literal_eval(lats)
                         # print("lats", lats, type(lats))
-                        assert (
-                            len(lats) == 1
-                        ), "Multi-instr sharing groups are unsupported!"
+                        assert len(lats) == 1, "Multi-instr sharing groups are unsupported!"
                     for instr_name, lat in lats.items():
                         sg2instrs[grp].append(instr_name)
                         lat_ = lat
@@ -358,9 +332,7 @@ def main():
                     # print("instr_name", instr_name)
                     # print("instr_latencies2", instr_latencies2)
                     instr_cycles = instr_latencies2[instr_name]
-                    sgs = [
-                        sg for sg, instrs in sg2instrs.items() if instr_name in instrs
-                    ]
+                    sgs = [sg for sg, instrs in sg2instrs.items() if instr_name in instrs]
                     # print("sgs", sgs)
                     assert len(sgs) == 1
                     sg = sgs[0]
@@ -385,7 +357,28 @@ def main():
                         "cv32e40p_xisaac_stages.part": "cv32e40p_xisaac_stages.mako",
                         "cv32e40p_xisaac_pipelines.part": "cv32e40p_xisaac_pipelines.mako",
                     },
+                    "cva6": {
+                        "cva6_xisaac_microactions.part": "cva6_xisaac_microactions_new.mako",
+                        "cva6_xisaac_resources.part": "cva6_xisaac_resources_new.mako",
+                        "cva6_xisaac_model.part": "cva6_xisaac_model_new.mako",
+                        "cva6_xisaac_stages.part": "cva6_xisaac_stages.mako",
+                        "cva6_xisaac_pipelines.part": "cva6_xisaac_pipelines.mako",
+                    },
                 }
+                trace_values = set()
+                for instr_name, instr_operands in instr_operands_map.items():
+                    reg_operands = {
+                        operand_name: data for operand_name, data in instr_operands.items() if data[1] == "REG"
+                    }
+                    imm_operands = {
+                        operand_name: data for operand_name, data in instr_operands.items() if data[1] == "IMM"
+                    }
+                    reg_names = list(reg_operands.keys()) + list(imm_operands.keys())
+                    for reg_name in reg_names:
+                        trace_values_ = {reg_name, f"{reg_name}_data"}
+                        trace_values.update(trace_values_)
+                print("trace_values", trace_values)
+                # input("!")
                 cores_parts_map2 = {
                     "cv32e40p": {
                         "cv32e40p_xisaac_microaction_mapping.part": "cv32e40p_xisaac_microaction_mapping_new.mako",
@@ -394,28 +387,34 @@ def main():
                         "cv32e40p_xisaac_trace_value_mapping.part": "cv32e40p_xisaac_trace_value_mapping.mako",
                         "cv32e40p_xisaac_virtual_microactions.part": "cv32e40p_xisaac_virtual_microactions_new.mako",
                         "cv32e40p_xisaac_virtual_resources.part": "cv32e40p_xisaac_virtual_resources_new.mako",
+                        "cv32e40p_xisaac_trace_values.part": "cv32e40p_xisaac_trace_values.mako",
+                    },
+                    "cva6": {
+                        "cva6_xisaac_microaction_mapping.part": "cva6_xisaac_microaction_mapping_new.mako",
+                        "cva6_xisaac_ex_stages.part": "cva6_xisaac_ex_stages_new.mako",
+                        "cva6_xisaac_instr_groups.part": "cva6_xisaac_instr_groups.mako",
+                        "cva6_xisaac_trace_value_mapping.part": "cva6_xisaac_trace_value_mapping.mako",
+                        "cva6_xisaac_virtual_microactions.part": "cva6_xisaac_virtual_microactions_new.mako",
+                        "cva6_xisaac_virtual_resources.part": "cva6_xisaac_virtual_resources_new.mako",
+                        "cva6_xisaac_trace_values.part": "cva6_xisaac_trace_values.mako",
                     },
                 }
                 core_parts_map = cores_parts_map.get(args.core)
                 core_parts_map2 = cores_parts_map2.get(args.core)
-                assert (
-                    core_parts_map is not None
-                ), f"Parts not found for core '{args.core}'"
-                assert (
-                    core_parts_map2 is not None
-                ), f"Parts not found for core '{args.core}'"
+                assert core_parts_map is not None, f"Parts not found for core '{args.core}'"
+                assert core_parts_map2 is not None, f"Parts not found for core '{args.core}'"
                 for part_file, part_tmpl in core_parts_map.items():
                     # print("template_dirs", template_dirs)
                     mylookup = TemplateLookup(directories=template_dirs)
-                    part_template = Template(
-                        filename=f"{templates_path}/parts/{part_tmpl}", lookup=mylookup
-                    )
+                    part_template = Template(filename=f"{templates_path}/parts/{part_tmpl}", lookup=mylookup)
                     part_content = part_template.render(
                         instr_names=instr_names,
                         instr_operands_map=instr_operands_map,
                         instrs_timing=instrs_timing,
                         sg2instrs=sg2instrs,
                         variant_name=variant_name,
+                        uarch_name=uarch_name,
+                        core_name=core_name,
                     )
                     if variant_name is not None:
                         subdir = dest_dir / variant_name
@@ -427,14 +426,15 @@ def main():
                         f.write(part_content)
                 for part_file, part_tmpl in core_parts_map2.items():
                     mylookup = TemplateLookup(directories=template_dirs)
-                    part_template = Template(
-                        filename=f"{templates_path}/parts/{part_tmpl}", lookup=mylookup
-                    )
+                    part_template = Template(filename=f"{templates_path}/parts/{part_tmpl}", lookup=mylookup)
                     part_content = part_template.render(
                         instr_names=instr_names,
                         instr_operands_map=instr_operands_map,
                         instrs_timing=instrs_timing,
                         sg2instrs=sg2instrs,
+                        uarch_name=uarch_name,
+                        core_name=core_name,
+                        trace_values=trace_values,
                     )
                     subdir = dest_dir
                     part_dest = subdir / part_file
@@ -457,18 +457,14 @@ def main():
                 template = args.template
                 if template is None:
                     assert core is not None
-                    core2template = {"cv32e40p": "CV32E40PXISAAC"}
+                    core2template = {"cv32e40p": "CV32E40PXISAAC", "cva6": "CVA6XISAAC"}
                     template = core2template.get(core)
-                    assert (
-                        template is not None
-                    ), f"Could not find template for core {core}"
-                template = lookup_template(
-                    template, core=args.core, suffix=".corePerfDsl"
-                )
+                    assert template is not None, f"Could not find template for core {core}"
+                template = lookup_template(template, core=args.core, suffix=".corePerfDsl")
                 # print("template", template)
                 assert template is not None
                 mytemplate = Template(filename=template, lookup=mylookup)
-                content = mytemplate.render(variants=variants, new=True)
+                content = mytemplate.render(variants=variants, new=True, uarch_name=uarch_name, core_name=core_name)
                 # if variant_name is not None:
                 #     header = f"// Variant: {variant_name}\n"
                 # else:
@@ -483,15 +479,9 @@ def main():
         if monitor_template is None:
             assert core is not None
             core2monitor_template = {}
-            monitor_template = core2monitor_template.get(
-                core, "InstructionTrace_XISAAC"
-            )
-            assert (
-                monitor_template is not None
-            ), f"Could not find monitor_template for core {core}"
-        monitor_template = lookup_template(
-            monitor_template, core=args.core, suffix=".json"
-        )
+            monitor_template = core2monitor_template.get(core, "InstructionTrace_XISAAC")
+            assert monitor_template is not None, f"Could not find monitor_template for core {core}"
+        monitor_template = lookup_template(monitor_template, core=args.core, suffix=".json")
         # print("monitor_template", monitor_template)
         monitor_template = Path(monitor_template)
         assert monitor_template.is_file()
@@ -517,7 +507,7 @@ def main():
             variant_suffix = variant_name if variant_name is not None else ""
             extras = variant_extras[variant_name]
             variant_description, variant_details, total_area_estimate = extras
-            uarch = f"CV32E40PXISAAC{variant_suffix}"
+            uarch = f"{uarch_name}{variant_suffix}"
             uarch_lower = uarch.lower()
             new = {
                 "uarch": uarch,
