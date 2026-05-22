a60d369fe57889a03b536af72ed4327deab5716e
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-05-22 18:52:41 +0200

    update 4-picojpeg/cv32e40p files

--- examples/4-picojpeg/cv32e40p/config.mk
+++ examples/4-picojpeg/cv32e40p/config.mk
@@ -4,9 +4,9 @@ ETISS_ARCH ?= XIsaacCore
 UARCH_NAME := CV32E40PXISAAC
 MONITOR_NAME := InstructionTrace_XISAAC
 
-INDEX_FILE := $(INPUTS_DIR)/final_index.yml
+INDEX_FILE := $(INPUTS_DIR)/filtered_selected_index.yml
 HLS_DIR := $(INPUTS_DIR)/hls
 
-INI_DL_URL := https://raw.githubusercontent.com/PhilippvK/isaac-artifacts/refs/heads/main/experiments/embench_iot/picojpeg/20250808T110607/run_compare_final/1/custom.ini
-ELF_DL_URL := https://github.com/PhilippvK/isaac-artifacts/raw/refs/heads/main/experiments/embench_iot/picojpeg/20250808T110607/run_compare_final/1/generic_mlonmcu
-ETISS_SRC_DL_URL := https://github.com/PhilippvK/isaac-artifacts/raw/refs/heads/main/experiments/embench_iot/picojpeg/20250808T110607/work/docker/etiss_final/etiss_source.zip
+INI_DL_URL := https://raw.githubusercontent.com/PhilippvK/isaac-artifacts/refs/heads/main/experiments/embench_iot/picojpeg/20260522T170540/run_new_perf_filtered_selected/custom.ini
+ELF_DL_URL := https://github.com/PhilippvK/isaac-artifacts/raw/refs/heads/main/experiments/embench_iot/picojpeg/20260522T170540/run_new_perf_filtered_selected/generic_mlonmcu
+ETISS_SRC_DL_URL := https://github.com/PhilippvK/isaac-artifacts/raw/refs/heads/main/experiments/embench_iot/picojpeg/20260522T170540/work/local/etiss_perf_filtered_selected/etiss_perf_source.tar.gz
