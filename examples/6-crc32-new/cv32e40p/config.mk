b2f338c86a32fce6c9b9c087f867713ce245c852
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-05-22 17:03:39 +0200

    add example 6-crc32-new

---
+++ examples/6-crc32-new/cv32e40p/config.mk
@@ -0,0 +1,12 @@
+CORE := cv32e40p
+ARCH_NAME ?= XIsaac
+ETISS_ARCH ?= XIsaacCore
+UARCH_NAME := CV32E40PXISAAC
+MONITOR_NAME := InstructionTrace_XISAAC
+
+INDEX_FILE := $(INPUTS_DIR)/filtered_selected_index.yml
+HLS_DIR := $(INPUTS_DIR)/hls
+
+INI_DL_URL := https://raw.githubusercontent.com/PhilippvK/isaac-artifacts/refs/heads/main/experiments/embench_iot/crc32/20260522T154826/run_new_perf_filtered_selected/custom.ini
+ELF_DL_URL := https://github.com/PhilippvK/isaac-artifacts/raw/refs/heads/main/experiments/embench_iot/crc32/20260522T154826/run_new_perf_filtered_selected/generic_mlonmcu
+ETISS_SRC_DL_URL := https://github.com/PhilippvK/isaac-artifacts/raw/refs/heads/main/experiments/embench_iot/crc32/20260522T154826/work/local/etiss_perf_filtered_selected/etiss_perf_source.tar.gz
